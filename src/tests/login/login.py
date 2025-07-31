1d6cec541cbf832dae1fd804efb2c7860f5b2939
nicolas <nicoezarate@gmail.com>
2025-07-31 20:15:25 -0300

    fixed

--- src/tests/login/login.py
+++ src/tests/login/login.py
@@ -31,3 +31,7 @@ class TestLoginHardCodeado:
        driver.find_element(By.ID, "user-name").send_keys("standard_user")
        driver.find_element(By.ID, "password").send_keys("secret_sauce")
        driver.find_element(By.ID, "login-button").click()
+
+       # Verificar que el login fue exitoso
+       products_text = driver.find_element(By.CLASS_NAME, "title").text
+       assert "Products" == products_text, f"Se esperaba 'Products' en el texto, pero se obtuvo: {products_text}"
